f102da619f0fedd93020501b82cac028361f781e
thiha <thiha.naing.ucsm@gmail.com>
2022-04-22 09:56:22 +0630

    Maple3

--- lms/static/js/student_account/enrollment.js
+++ lms/static/js/student_account/enrollment.js
@@ -16,7 +16,7 @@
              * @param  {string} courseKey  Slash-separated course key.
              * @param  {string} redirectUrl The URL to redirect to once enrollment completes.
              */
-            enroll: function(courseKey, redirectUrl, chapterId, sectionId) {
+            enroll: function(courseKey, redirectUrl) {
                 var data_obj = {course_id: courseKey},
                     data = JSON.stringify(data_obj);
 
@@ -37,24 +37,18 @@
                         this.redirect(responseData.user_message_url);
                     } else {
                         // Otherwise, redirect the user to the next page.
-                        //if (redirectUrl) {
-                            //this.redirect(redirectUrl);
-                        //}
-                        this.redirectToSectionDetail(courseKey, chapterId, sectionId)
+                        if (redirectUrl) {
+                            this.redirect(redirectUrl);
+                        }
                     }
                 }).done(function(response) {
                     // If we successfully enrolled, redirect the user
                     // to the next page (usually the student dashboard or payment flow)
-
-                    this.redirectToSectionDetail(courseKey, chapterId, sectionId)
-
-                    /**
-                     * if (response.redirect_destination) {
+                    if (response.redirect_destination) {
                         this.redirect(response.redirect_destination);
                     } else if (redirectUrl) {
                         this.redirect(redirectUrl);
                     }
-                     */
                 });
             },
 
@@ -62,14 +56,6 @@
              * Redirect to a URL.  Mainly useful for mocking out in tests.
              * @param  {string} url The URL to redirect to.
              */
-
-            redirectToSectionDetail: function(courseKey, chapterId, sectionId) {
-
-                var url = `${window.location.origin}/courses/${courseKey}/courseware/${chapterId}/${sectionId}`
-                window.location.href = url
-
-            },
-
             redirect: function(url) {
                 window.location.href = url;
             }
